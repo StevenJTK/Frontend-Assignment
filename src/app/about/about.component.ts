7295433867d8d68b0b9f0e4c8121a4e937c9d73e
StevenJTK <114819398+StevenJTK@users.noreply.github.com>
2025-02-18 16:59:32 +0100

    Third Commit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Character } from '../models/character';
+import { getCharacters } from '../heroes/heroes.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-about',
-  imports: [RouterLink],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  characters: Character[] = [];
 
+  ngOnInit(): void {
+    this.characters = getCharacters();
+  }
 }
+
+
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Character } from '../models/character';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-heroes',
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  characters: Character[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.characters = getCharacters();
+  }
+}
+
+const MoonKnight: Character = {
+  name: "Marc Spector",
+  power: "Blessed by Khonsu",
+  age: 37,
+  isEvil: false,
+  isAvenger: false
+}
+
+const Storm: Character = {
+  name: "Ororo Munroe",
+  power: "Weather Control",
+  age: 32,
+  isEvil: false,
+  isAvenger: true
+}
+
+const Loki: Character = {
+  name: "Loki Laufeyson",
+  power: "Trickster God",
+  age: 1050,
+  isEvil: true,
+  isAvenger: false
+}
+
+
+export function getCharacters(): Character[] {
+  return [MoonKnight, Storm, Loki]
+}
